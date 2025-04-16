f7157f58c3ac05874a2beeff581b53905f8121cd
Nikodem <nikodemmlynski08@gmail.com>
2025-04-17 00:12:40 +0200

    🔧 Refaktoryzacja hooków users oraz dodanie uniwersalnego hooka optimistic update
    - Poprawiono hooki  i , aby poprawnie komunikowały się z API.
    - Dodano uniwersalny hook , umożliwiający obsługę optimistic update i zarządzanie stanem lokalnym użytkowników.
    - Dzięki temu kod jest bardziej skalowalny, DRY i gotowy do wykorzystania w różnych kontekstach (nauczyciele, uczniowie, rodzice).

---
+++ admin_panel/src/hooks/users.ts
@@ -0,0 +1,74 @@
+import { API_URL } from "@/config/constants";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+type UpdateUserInput = {
+    first_name: string;
+    last_name: string;
+    email: string;
+}
+
+async function fetcher<T>(
+    url: string,
+    token?: string,
+    options?: RequestInit
+): Promise<T> {
+    const res = await fetch(url, {
+        headers: {
+            "Content-Type": "application/json",
+            ...(token ? {Authorization: `Bearer ${token}`} : {})
+        },
+        ...options
+    })
+
+    if(!res.ok) {
+        throw new Error(await res.text());
+    }
+
+    return res.json();
+}
+
+export function useUsers<T>(endpoint: string, token?: string) {
+    return useQuery<T[]>({
+        queryKey: [endpoint], // to będzie trzeba zmienić na user i role
+        queryFn: () => fetcher<T[]>(`${API_URL}${endpoint}`, token),
+    });
+}
+
+export function useUser<T>(endpoint: string, id: number, token?: string) {
+    return useQuery<T>({
+        queryKey: [endpoint, id], // tak samo,
+        queryFn: () => fetcher<T>(`${API_URL}${endpoint}${id}`, token),
+        enabled: !!id,
+    })
+}
+
+export function useUpdateUser<T>(endpoint: string, token?: string) {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: async ({ id, data }: { id: number; data: UpdateUserInput }) => {
+            return fetcher<T>(`${API_URL}${endpoint}/${id}`, token, {
+                method: 'PUT',
+                body: JSON.stringify(data),
+            })
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: [endpoint]})
+        } 
+    })
+}
+
+export function useDeleteUser(endpoint: string, token?: string) {
+    const queryClient = useQueryClient();
+  
+    return useMutation({
+      mutationFn: async (id: number) => {
+        return fetcher(`${API_URL}${endpoint}/${id}`, token, {
+          method: 'DELETE',
+        });
+      },
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: [endpoint] });
+      },
+    });
+  }
