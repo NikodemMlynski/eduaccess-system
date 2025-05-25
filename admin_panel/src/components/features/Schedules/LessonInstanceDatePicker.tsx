4049ce4ab37236dcbfa385d6ad07a00a1b266edb
Nikodem <nikodemmlynski@gmail.com>
2025-05-26 01:42:14 +0200

    dodanie możliwości poruszania się tydzień do przodu oraz do tyłu w date pickerze

--- admin_panel/src/components/features/Schedules/LessonInstanceDatePicker.tsx
+++ admin_panel/src/components/features/Schedules/LessonInstanceDatePicker.tsx
@@ -1,6 +1,7 @@
 import {useNavigate} from "react-router-dom";
 import {Button} from "@/components/ui/button.tsx";
-
+import {ArrowLeft, ArrowRight} from "lucide-react"
+import {useEffect, useState} from "react";
 interface ILessonInstanceDatePickerProps {
     dateStr?: string;
     type: "classes" | "teachers" | "rooms";
@@ -11,7 +12,7 @@ const LessonInstanceDatePicker = ({
     type,
     id
 }: ILessonInstanceDatePickerProps) => {
-    const date = new Date( dateStr || "");
+    const [date, setDate] = useState(new Date( dateStr || ""));
     const monday = date.getDay() - 1;
     const mondayDate = new Date(new Date(date).setDate(new Date(date).getDate() - monday));
     const weekdaysArr = [];
@@ -21,8 +22,23 @@ const LessonInstanceDatePicker = ({
         weekdaysArr.push(day);
     }
     const navigate = useNavigate();
+
+    const handleDateChange = (type: "add" | "sub") => {
+
+        setDate(prevDate => {
+            const newDate = new Date(prevDate);
+            newDate.setDate(prevDate.getDate() + (type === "add" ? 7 : -7));
+            return newDate;
+        })
+    }
+    useEffect(() => {
+        navigate(`/schedules/${type}/${id}/dateStr/${date.toISOString().split("T")[0]}`)
+    }, [date])
     return (
-        <div className="flex justify-center space-x-2 py-2 mt-5">
+        <div className="flex justify-center items-center space-x-2 py-2 mt-5 relative">
+            <ArrowLeft className="cursor-pointer absolute left-4"
+            onClick={() => handleDateChange("sub")}
+            />
         {
             weekdaysArr.map((day, index) => (
                 <Button className={`cursor-pointer rounded-[50%] w-8 h-8 flex items-center justify-center
@@ -36,6 +52,9 @@ const LessonInstanceDatePicker = ({
                 >{day.getDate()}</Button>
             ))
         }
+        <ArrowRight className="cursor-pointer absolute right-4"
+            onClick={() => handleDateChange("add")}
+        />
         </div>
     )
 }
