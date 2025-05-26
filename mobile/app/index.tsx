aa32daf3b824a97da43dad4cd371759106d414af
Nikodem <nikodemmlynski08@gmail.com>
2025-05-26 21:29:36 +0200

    stworzenie welcome screena

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,13 +1,32 @@
-import { Text, View } from "react-native";
-import {Link} from "expo-router";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <View
-      className="bg-red-200"
-    >
-      <Text className="text-2xl">Edit app/index.tsx to edit this screen.</Text>
-      <Link href={"/(auth)/sign-up"}>Zarejestruj się</Link>
+    <View className="flex-1 bg-background">
+      <View className="mt-5 h-[68%] w-full">
+        <Image
+          source={require("@/assets/images/welcome_screen.png")}
+          className="w-full h-full"
+          resizeMode="cover"
+        />
+      </View>
+
+      <View className="flex-1 items-center justify-start px-6 pt-20 gap-5">
+        {/* Przycisk Sign Up */}
+        <Link href="(auth)/sign-up" asChild>
+          <TouchableOpacity className="w-full bg-primary py-4 rounded-2xl shadow-md mb-4">
+            <Text className="text-white text-center text-lg font-semibold">Zarejestruj się</Text>
+          </TouchableOpacity>
+        </Link>
+
+        {/* Link Login */}
+        <Link href="(auth)/sign-in" asChild>
+          <TouchableOpacity>
+            <Text className="text-accent text-base font-medium">Mam już konto</Text>
+          </TouchableOpacity>
+        </Link>
+      </View>
     </View>
   );
 }
