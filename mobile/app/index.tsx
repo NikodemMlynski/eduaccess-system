440217d239ae940f1e7578270bee5c328017d764
Nikodem <nikodemmlynski08@gmail.com>
2025-05-26 18:52:41 +0200

    zainicjalizowanie projektu aplikacji mobilnej w expo + nativewind

---
+++ mobile/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="bg-red-200"
+    >
+      <Text className="text-2xl">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
