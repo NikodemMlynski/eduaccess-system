2069456c8b4847187ccfe329152fbecf623f9251
Nikodem <nikodemmlynski08@gmail.com>
2025-05-29 22:25:10 +0200

    stworzenie bottom navigation bara, który zawiera linki do screenów aplikacji po których mogą poruszać się użytkownicy, póki co strony są puste

--- mobile/app/(root)/(student)/profile.tsx
+++ mobile/app/(root)/(student)/profile.tsx
@@ -4,23 +4,22 @@ import {useSchool} from "@/hooks/school";
 import Profile from "@/components/Profile"
 import {useStudent} from "@/hooks/students";
 const Index = () => {
-  const {user, logout, token } = useAuth();
-  console.log(user);
-    const {data: teacher, isLoading: teacherIsLoading,
-        isError: teacherIsError, error: teacherError
-    } = useStudent(`school/${user?.school_id}/students`, user?.id, token || "");
+    const {user, logout, token } = useAuth();
+    const {data: school, isLoading, isError, error} = useSchool(`school`, user?.school_id, token || "");
+    if (isLoading) return <Text>Loading...</Text>;
+    if (isError) return <Text>Error: {error.message}</Text>;
 
-   const {data: school, isLoading, isError, error} = useSchool(`school`, user?.school_id, token || "");
-   if (isLoading) return <Text>Loading...</Text>;
-  if (isError) return <Text>Error: {error.message}</Text>;
-    console.log(teacher);
-  return (
-    <Profile
-        user={user}
-        logout={logout}
-        school={school || null}
-    />
-  );
+     // const {data: teacher, isLoading: teacherIsLoading,
+     //    isError: teacherIsError, error: teacherError
+     // } = useStudent(`school/${user?.school_id}/students`, user?.id, token || "");
+
+    return (
+        <Profile
+            user={user}
+            logout={logout}
+            school={school || null}
+        />
+    );
 };
 
 export default Index;
---
+++ mobile/app/(root)/(teacher)/door_requests.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from "react-native";
+const Index = () => {
+    return (
+        <View className="flex items-center justify-center">
+            <Text>door_requests</Text>
+        </View>
+    );
+};
+
+export default Index;
