47427dd3d3839742f48a6ef85933c486b156ea7b
Nikodem <nikodemmlynski08@gmail.com>
2025-05-30 15:46:04 +0200

    dodanie date pickera z który umożliwia wybranie dnia dla którego pobrać lekcje, wyróżnienie aktualnej lekcji w liście

--- mobile/app/(root)/(student)/schedule.tsx
+++ mobile/app/(root)/(student)/schedule.tsx
@@ -1,18 +1,23 @@
-import {View, Text, SafeAreaView} from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
+import { useState } from "react";
 import { useStudent } from "@/hooks/students";
 import { useLessonInstance } from "@/hooks/schedule";
 import { useAuth } from "@/context/AuthContext";
 import LessonInstancesMobile from "@/components/schedule/LessonList";
+import LessonInstanceDatePickerMobile from "@/components/schedule/LessonInstanceDayPicker";
+import { format } from "date-fns";
 
 const ScheduleScreen = () => {
   const { user, token } = useAuth();
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const {
     data: student,
     isLoading: studentIsLoading,
     isError: studentIsError,
   } = useStudent(`school/${user?.school_id}/students`, user?.id, token || "");
 
-  const dateStr = "2025-05-29";
+  const formattedDateStr = format(selectedDate, "yyyy-MM-dd");
 
   const {
     data: lessons,
@@ -23,16 +28,21 @@ const ScheduleScreen = () => {
     "classes",
     token || "",
     Number(student?.class_.id),
-    dateStr
+    formattedDateStr
   );
+  let content = <Text>Loading...</Text>
 
-  if (studentIsLoading || lessonIsLoading) return <Text>Loading...</Text>;
-  if (studentIsError || lessonIsError) return <Text>Error loading data</Text>;
-
+  if (studentIsLoading || lessonIsLoading) content =  <Text>Loading...</Text>;
+  if (studentIsError || lessonIsError) content = <Text>Error loading data</Text>;
+  content = <LessonInstancesMobile lesson_instances={lessons || []} />
+  if (lessons?.length == 0) content = <View className="h-[70%] w-full flex items-center justify-center"><Text className="text-red-200 text-2xl font-light">Brak lekcji</Text></View>
   return (
-    <SafeAreaView className="flex-1 bg-black py-12">
-      <Text className="text-2xl text-center py-2 text-green-text">Plan lekcji</Text>
-      <LessonInstancesMobile lesson_instances={lessons} />
+    <SafeAreaView className="flex-1 bg-black py-0">
+      <View className="flex flex-row justify-between px-5 py-3 pt-12 bg-background">
+        <Text className="text-2xl text-center py-2 text-white">Plan lekcji</Text>
+      </View>
+      <LessonInstanceDatePickerMobile selectedDate={selectedDate} setSelectedDate={setSelectedDate} />
+      {content}
     </SafeAreaView>
   );
 };
--- mobile/components/schedule/LessonCard.tsx
+++ mobile/components/schedule/LessonCard.tsx
@@ -1,14 +1,21 @@
 import { View, Text } from "react-native";
 import { ILessonInstance } from "@/types/schedule";
-import { format } from "date-fns";
+import { format, isWithinInterval } from "date-fns";
 
 interface LessonInstanceMobileProps {
   lesson: ILessonInstance;
 }
 
 const LessonInstanceMobile = ({ lesson }: LessonInstanceMobileProps) => {
+    const now = new Date();
+
+  const isCurrentLesson = isWithinInterval(now, {
+    start: new Date(lesson.start_time + "Z"),
+    end: new Date(lesson.end_time + "Z"),
+  });
+  console.log(isCurrentLesson);
   return (
-    <View className="w-full px-4 py-3 border-b border-[#444]">
+    <View className={`w-full px-4 py-3 border-b ${isCurrentLesson ? "bg-surface/70 border-[#00CFFF]" : "border-[#444]"}`}>
       <Text className="text-white font-semibold text-lg mb-1">
         {lesson.subject}
       </Text>
@@ -18,7 +25,7 @@ const LessonInstanceMobile = ({ lesson }: LessonInstanceMobileProps) => {
         {format(lesson.start_time, "HH:mm")} - {format(lesson.end_time, "HH:mm")}
       </Text>
 
-      <Text className="text-green-400 text-sm mb-0.5 min-w-[55px]">
+      <Text className="text-accent text-sm mb-0.5 min-w-[55px]">
         Sala: {lesson.room.room_name}
       </Text>
 
