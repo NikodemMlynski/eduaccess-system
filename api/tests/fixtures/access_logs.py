f1d01caf3d64202906caa001760dbb4ed5f143f7
Nikodem <nikodemmlynski08@gmail.com>
2025-06-19 16:34:33 +0200

    napisanie testów dla endpointu /request, dla przypadków: udzielono uprawnień zgodnie z planem lekcji błąd związany z nieprzypisaniem ucznia do żadnej klasy odrzucono zgodnie z planem lekcji - lekcja nie istnieje odrzucono zgodnie z planem lekcji - uczeń nie jest przypisany do klasy która ma lekcje w danej sali i o danej godzinie access log już istnieje - zmofydikowania access_end_time niepoprawne dane wejściowe niezgodne uprawenienia - użytkownik zalogowany jako nauczyciel

--- api/tests/fixtures/access_logs.py
+++ api/tests/fixtures/access_logs.py
@@ -2,18 +2,25 @@ import pytest
 from datetime import datetime
 from app.models import AccessLog
 from sqlalchemy.orm import Session
+from typing import Optional
 
 @pytest.fixture
 def access_logs_factory(session: Session):
     def create_access_log(
         user_id: int,
         room_id: int,
-        access_time: datetime
+        access_start_time: datetime,
+        access_status: str,
+        access_end_time: Optional[datetime] = None,
+        reason: Optional[str] = None,
     ):
         access_log = AccessLog(
             user_id=user_id,
             room_id=room_id,
-            access_time=access_time
+            access_start_time=access_start_time,
+            access_end_time=access_end_time,
+            access_status=access_status,
+            reason=reason,
         )
         session.add(access_log)
         session.commit()
--- api/tests/test_access_logs.py
+++ api/tests/test_access_logs.py
@@ -62,14 +62,292 @@ def test_request_access_log_student_granted_by_schedule(
 
     res = client.post(f"/school/{school_id}/access-logs/request", json=payload)
     data = res.json()
-    print(data)
     assert res.status_code == 200
+    assert data["access_status"] == "granted"
+
+
+
 # 2. request_access_log_student_not_assigned_to_any_class
+def test_request_access_log_student_not_assigned_to_any_class(
+        authorized_student_client,
+        session,
+        lesson_template_factory,
+        lesson_instance_factory,
+        room_factory,
+        user_factory,
+        classes_factory,
+):
+    student, client = authorized_student_client
+    school_id = student.user.school_id
+    room = room_factory(school_id=school_id)
+    klass = classes_factory(school_id=school_id, class_name="4D")
+    teacher = user_factory(school_id=school_id, role='teacher', email="teacher1@example.com")
+    template = lesson_template_factory(
+        school_id=school_id,
+        room_id=room.id,
+        teacher_id=teacher.id,
+        class_id=klass.id,
+        start_time="10:05",
+        end_time="10:50",
+        weekday=0
+    )
+
+
+    start_time = datetime(year=2025, month=5, day=15, hour=10, minute=5)
+    end_time = datetime(year=2025, month=5, day=15, hour=10, minute=50)
+
+    lesson_instance = lesson_instance_factory(
+        template_id=template.id,
+        class_id=klass.id,
+        room_id=room.id,
+        teacher_id=teacher.id,
+        subject="computer science",
+        start_time=start_time,
+        end_time=end_time,
+    )
+    payload = {
+        "user_id": student.user.id,
+        "room_id": room.id,
+        "access_time": start_time.isoformat()
+    }
+
+    res = client.post(f"/school/{school_id}/access-logs/request", json=payload)
+    data = res.json()
+    print(data)
+    assert res.status_code == 400
+    assert data["detail"] == f"Student {student.user.id} is not assigned to any class."
 
 # 3. request_access_log_student_denied_by_schedule
+def test_request_access_log_student_denied_by_schedule_lesson_not_existing(
+        authorized_student_class4D_client,
+        session,
+        lesson_template_factory,
+        lesson_instance_factory,
+        room_factory,
+        user_factory,
+        classes_factory,
+):
+    student, klass, client = authorized_student_class4D_client
+    school_id = student.user.school_id
+    room = room_factory(school_id=school_id)
+
+    start_time = datetime(year=2025, month=5, day=15, hour=10, minute=5)
+
+    payload = {
+        "user_id": student.user.id,
+        "room_id": room.id,
+        "access_time": start_time.isoformat()
+    }
+
+    res = client.post(f"/school/{school_id}/access-logs/request", json=payload)
+    data = res.json()
+    assert res.status_code == 200
+    assert data["access_status"] == "denied"
+
+def test_request_access_log_student_denied_by_schedule_student_not_assigned_to_class_having_lesson(
+        authorized_student_class4D_client,
+        session,
+        lesson_template_factory,
+        lesson_instance_factory,
+        room_factory,
+        user_factory,
+        classes_factory,
+):
+    student, _, client = authorized_student_class4D_client
+    school_id = student.user.school_id
+    klass = classes_factory(school_id=school_id, class_name="3C")
+    room = room_factory(school_id=school_id)
+    teacher = user_factory(school_id=school_id, role='teacher', email="teacher1@example.com")
+    template = lesson_template_factory(
+        school_id=school_id,
+        room_id=room.id,
+        teacher_id=teacher.id,
+        class_id=klass.id,
+        start_time="10:05",
+        end_time="10:50",
+        weekday=0
+    )
+
+
+    start_time = datetime(year=2025, month=5, day=15, hour=10, minute=5)
+    end_time = datetime(year=2025, month=5, day=15, hour=10, minute=50)
+
+    lesson_instance = lesson_instance_factory(
+        template_id=template.id,
+        class_id=klass.id,
+        room_id=room.id,
+        teacher_id=teacher.id,
+        subject="computer science",
+        start_time=start_time,
+        end_time=end_time,
+    )
+    payload = {
+        "user_id": student.user.id,
+        "room_id": room.id,
+        "access_time": start_time.isoformat()
+    }
+
+    res = client.post(f"/school/{school_id}/access-logs/request", json=payload)
+    data = res.json()
+    assert res.status_code == 200
+    assert data["access_status"] == "denied"
 # 4. request_access_log_already_exists
+
+def test_request_access_log_student_access_log_already_exists(
+        authorized_student_class4D_client,
+        session,
+        lesson_template_factory,
+        lesson_instance_factory,
+        room_factory,
+        user_factory,
+        classes_factory,
+        access_logs_factory,
+):
+    student, klass, client = authorized_student_class4D_client
+    school_id = student.user.school_id
+    room = room_factory(school_id=school_id)
+    teacher = user_factory(school_id=school_id, role='teacher', email="teacher1@example.com")
+    template = lesson_template_factory(
+        school_id=school_id,
+        room_id=room.id,
+        teacher_id=teacher.id,
+        class_id=klass.id,
+        start_time="10:05",
+        end_time="10:50",
+        weekday=0
+    )
+
+
+    start_time = datetime(year=2025, month=5, day=15, hour=10, minute=5)
+    end_time = datetime(year=2025, month=5, day=15, hour=10, minute=50)
+
+    lesson_instance = lesson_instance_factory(
+        template_id=template.id,
+        class_id=klass.id,
+        room_id=room.id,
+        teacher_id=teacher.id,
+        subject="computer science",
+        start_time=start_time,
+        end_time=end_time,
+    )
+    access_log = access_logs_factory(
+        access_start_time=start_time.isoformat(),
+        room_id=room.id,
+        user_id=student.user.id,
+        access_status="granted",
+        reason="Student currently have lesson in this classroom"
+    )
+    access_time = datetime(year=2025, month=5, day=15, hour=10, minute=7)
+
+    payload = {
+        "user_id": student.user.id,
+        "room_id": room.id,
+        "access_time": access_time.isoformat()
+    }
+
+    res = client.post(f"/school/{school_id}/access-logs/request", json=payload)
+    data = res.json()
+    print(data)
+    assert res.status_code == 200
+    assert data["access_status"] == "granted"
+    assert data["access_end_time"] == access_time.isoformat()
+
 # 5. request_access_log_invalid_data
+def test_request_access_log_student_uncomplete_data(
+        authorized_student_class4D_client,
+        session,
+        lesson_template_factory,
+        lesson_instance_factory,
+        room_factory,
+        user_factory,
+        classes_factory,
+):
+    student, klass, client = authorized_student_class4D_client
+    school_id = student.user.school_id
+    room = room_factory(school_id=school_id)
+    teacher = user_factory(school_id=school_id, role='teacher', email="teacher1@example.com")
+    template = lesson_template_factory(
+        school_id=school_id,
+        room_id=room.id,
+        teacher_id=teacher.id,
+        class_id=klass.id,
+        start_time="10:05",
+        end_time="10:50",
+        weekday=0
+    )
+
+
+    start_time = datetime(year=2025, month=5, day=15, hour=10, minute=5)
+    end_time = datetime(year=2025, month=5, day=15, hour=10, minute=50)
+
+    lesson_instance = lesson_instance_factory(
+        template_id=template.id,
+        class_id=klass.id,
+        room_id=room.id,
+        teacher_id=teacher.id,
+        subject="computer science",
+        start_time=start_time,
+        end_time=end_time,
+    )
+    payload = {
+        "user_id": student.user.id,
+        "room_id": room.id,
+    }
+
+    res = client.post(f"/school/{school_id}/access-logs/request", json=payload)
+    data = res.json()
+    print(data)
+    assert res.status_code == 422
+    assert data["detail"] == [{'input': {'room_id': 1, 'user_id': 2}, 'loc': ['body', 'access_time'], 'msg': 'Field required', 'type': 'missing'}]
+
 # 6. request_access_log_provided_user_is_not_student
+def test_request_access_log_provided_user_is_not_student(
+        authorized_teacher_client,
+        session,
+        lesson_template_factory,
+        lesson_instance_factory,
+        room_factory,
+        user_factory,
+        classes_factory,
+):
+    student, client = authorized_teacher_client
+    school_id = student.user.school_id
+    klass = classes_factory(school_id=school_id, class_name="4D")
+    room = room_factory(school_id=school_id)
+    teacher = user_factory(school_id=school_id, role='teacher', email="teacher1@example.com")
+    template = lesson_template_factory(
+        school_id=school_id,
+        room_id=room.id,
+        teacher_id=teacher.id,
+        class_id=klass.id,
+        start_time="10:05",
+        end_time="10:50",
+        weekday=0
+    )
+
+
+    start_time = datetime(year=2025, month=5, day=15, hour=10, minute=5)
+    end_time = datetime(year=2025, month=5, day=15, hour=10, minute=50)
+
+    lesson_instance = lesson_instance_factory(
+        template_id=template.id,
+        class_id=klass.id,
+        room_id=room.id,
+        teacher_id=teacher.id,
+        subject="computer science",
+        start_time=start_time,
+        end_time=end_time,
+    )
+    payload = {
+        "user_id": student.user.id,
+        "room_id": room.id,
+    }
+
+    res = client.post(f"/school/{school_id}/access-logs/request", json=payload)
+    data = res.json()
+    assert res.status_code == 403
+    assert data["detail"] == "Insufficient permissions"
+
 
 # 7. get_all_denied_access_logs_for_teacher
 # 8. get_all_denied_access_logs_for_teacher_time_edge_case
